7f534a02be616dc2c595e76a979e4cdebf89f473
Sean Madden <sdm350@gmail.com>
2015-09-30 22:30:08 -0700

    Changed to use Object.create

--- parser.js
+++ parser.js
@@ -1,32 +1,23 @@
 'use strict';
 //min hour dom moy dow
-function Base_expression() {
-    this.minute = "*";
-    this.hour = "*";
-    this.day_of_month = "*";
-    this.month_of_year = "*";
-    this.day_of_week = "*";
-}
-
-Base_expression.prototype.toString = function() {
-    return `${this.minute} ${this.hour} ${this.day_of_month} ${this.month_of_year} ${this.day_of_week}`
-};
-
-Base_expression.prototype.setProp = function(prop, value) {
-    if (this.hasOwnProperty(prop)) {
-        this[prop] = value.trim();
-    } else {
-        throw Error("Invalid property name");
+const Base_expression = {
+    minute: "*",
+    hour: "*",
+    day_of_month: "*",
+    month_of_year: "*",
+    day_of_week: "*",
+    toString: function() {
+        return `${this.minute} ${this.hour} ${this.day_of_month} ${this.month_of_year} ${this.day_of_week}`;
+    },
+    setProp: function(prop, value) {
+        if (this[prop] !== 'underfined') {
+            this[prop] = value.trim();
+        } else {
+            throw Error("Invalid property name: " + prop);
+        }
     }
 };
 
-//const base_expression = {
-//    "minute": "*",
-//    "hour": "*",
-//    "day_of_month": "*",
-//    "month_of_year": "*",
-//    "day_of_week": "*"
-//};
 const DAYS_OF_WEEK = {
     'sunday': '0',
     'monday': '1',
@@ -47,13 +38,10 @@ var parser = function (english) {
 
 
     //TODO: create a new object from the base_expression
-    //let expression = Object.create({}, base_expression);
-    //console.log(expression);
-    let expression = new Base_expression();
+    let expression = Object.create(Base_expression);
 
-    //TODO: see if I can graby every <day_of_week> - I forget the syntax right now
     let pattern = "((mon|tues|wednes|thurs|fri|satur|sun)day)";
-    let frequency = pattern.exec(english);
+    let frequency = english.match(pattern);
     if (frequency) {
         frequency = frequency[0];
         expression.setProp('day_of_week', DAYS_OF_WEEK[frequency]);
